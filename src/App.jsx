402de5ba0e39531e39e5d3f3c4f6ee0c1cc3b4ea
Deeya <guptadeeya12@gmail.com>
2023-06-26 22:50:23 +0530

    React Router Dom setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+import React from 'react'
+import {BrowserRouter as Router,
+Route,
+Routes
+} from 'react-router-dom'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Netflix from './pages/Netflix';
+
+export default function App() {
+  return (
+    <Router> 
+      <Routes>
+        <Route exact path='/login' element={<Login/>}/>
+        <Route exact path='/signup' element={<Signup/>}/>
+        <Route exact path='/' element={<Netflix/>}/>
+      </Routes>
+    </Router>
+  );
+}
